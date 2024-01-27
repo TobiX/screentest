e98e16971e175083a157206cdf5a90a488bb6f57
markuspg <markuspg@users.noreply.github.com>
2024-01-27 16:50:11 +0100

    Port remaining test case "text" to Cairo

--- test_text.c
+++ test_text.c
@@ -25,71 +25,75 @@
 
 #include "callbacks.h"
 
-static gchar *fontnames[] = {
-    "-adobe-times-medium-r-normal-*-*-80-*-*-p-*-iso8859-1",
-    "-adobe-times-medium-r-normal-*-*-100-*-*-p-*-iso8859-1",
-    "-adobe-times-medium-r-normal-*-*-120-*-*-p-*-iso8859-1",
-    "-adobe-times-medium-r-normal-*-*-140-*-*-p-*-iso8859-1",
-    "-adobe-times-medium-r-normal-*-*-180-*-*-p-*-iso8859-1",
-    "-adobe-times-medium-r-normal-*-*-240-*-*-p-*-iso8859-1",
-    NULL};
+static gint font_sizes[] = {8, 10, 12, 14, 18, 24};
 
-static gint font_num;
+static gint font_size_num;
 
 static gchar text[] =
     "The Screentest home page: https://tobix.github.io/screentest/";
 
-GdkFont *font;
-gint baselineskip, textwidth;
-
-static void font_init(G_GNUC_UNUSED GtkWidget *widget) {
-  gint lbear, rbear, width, asc, desc;
-
-  font = gdk_font_load(fontnames[font_num]);
-
-  if (!font) {
-    printf("Cannot load font %s, trying 'fixed'.\n", fontnames[font_num]);
-    font = gdk_font_load("fixed");
-  }
-
-  gdk_string_extents(font, text, &lbear, &rbear, &width, &asc, &desc);
-  baselineskip = 6 * (asc + desc) / 5; /* 1.2x text height */
-  textwidth = width + baselineskip;    /* the actual width plus space */
-}
-
-static void text_init(GtkWidget *widget) {
-  font_num = 1;
-  font_init(widget);
-}
+static void text_init(GtkWidget *widget) { font_size_num = 1; }
 
 static void text_draw(GtkWidget *widget) {
+  cairo_t *cr;
+  PangoFontDescription *pft;
+  PangoLayout *pl;
   GdkWindow *win = gtk_widget_get_window(widget);
+  GString *dtext;
+  gint i;
+  PangoRectangle ink_rect;
   gint w, h;
-  gint x, y;
+  gint repetitions;
 
   h = gdk_window_get_height(win);
   w = gdk_window_get_width(win);
 
-  x = w + textwidth;
-  for (y = 0; y < h; y += baselineskip)
-    for (x -= w + textwidth; x < w; x += textwidth)
-      gdk_draw_string(win, font, gc, x, y, text);
-}
+  cr = gdk_cairo_create(gtk_widget_get_window(widget));
+  cairo_set_line_width(cr, 1.0);
 
-static void text_close(G_GNUC_UNUSED GtkWidget *widget) {
-  gdk_font_unref(font);
+  pft = pango_font_description_new();
+  pango_font_description_set_size(
+      pft, pango_units_from_double(font_sizes[font_size_num]));
+
+  pl = pango_cairo_create_layout(cr);
+  pango_layout_set_font_description(pl, pft);
+  pango_layout_set_justify(pl, TRUE);
+  pango_layout_set_width(pl, pango_units_from_double(w));
+
+  set_color_bg(cr);
+  cairo_paint(cr);
+
+  set_color_fg(cr);
+
+  pango_layout_set_text(pl, text, -1);
+  pango_layout_get_extents(pl, &ink_rect, NULL);
+  repetitions = (w * h) / (pango_units_to_double(ink_rect.width) *
+                           pango_units_to_double(ink_rect.height));
+
+  dtext = g_string_sized_new(repetitions * (strlen(text) + 1));
+  dtext = g_string_append(dtext, text);
+  for (i = 0; i < repetitions; ++i) {
+    dtext = g_string_append_c(dtext, ' ');
+    dtext = g_string_append(dtext, text);
+  }
+  pango_layout_set_text(pl, dtext->str, -1);
+  pango_cairo_show_layout(cr, pl);
+
+  cairo_destroy(cr);
+  cr = NULL;
+
+  pango_font_description_free(pft);
+  pft = NULL;
 }
 
 static void text_cycle(GtkWidget *widget) {
-  gdk_font_unref(font);
-  if (fontnames[++font_num] == NULL)
-    font_num = 0;
-  font_init(widget);
+  if (++font_size_num == (sizeof(font_sizes) / sizeof(font_sizes[0])))
+    font_size_num = 0;
 }
 
 G_MODULE_EXPORT struct test_ops text_ops = {
     .init = text_init,
     .draw = text_draw,
     .cycle = text_cycle,
-    .close = text_close,
+    .close = NULL,
 };
